689033c90ea74c6002faa3b57e031533d34e446e
shruthiyasam <shruthiyasam83@gmail.com>
2019-08-28 10:36:56 +0530

    commit

---
+++ abc/src/abc/Add.java
@@ -0,0 +1,13 @@
+package abc;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=90;
+		System.out.println(a+b);
+
+	}
+
+}
